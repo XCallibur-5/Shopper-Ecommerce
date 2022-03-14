9dd6f924717c0b8acde9b7d07de5d0884751a2ec
P <anveshprakhar@outlook.com>
2022-03-14 21:29:12 +0530

    here registration of new user has been dealt with

--- my-react-app/src/Pages/Register.jsx
+++ my-react-app/src/Pages/Register.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Footer from '../Components/Footer';
 import Navbars from '../Components/Navbars';
+import { publicRequest } from "../requestMethods";
+
 
 function Register(){
+    const [username,setuserName] = useState("");
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const Ruser ={
+        username: username,
+        email: email,
+        password: password,
+    }
+    console.log(Ruser);
+    const regis =async (e)=>{
+        e.preventDefault();
+        const k = await publicRequest.post("/auth/registration", Ruser);
+        window.location.href='/'
+    }
+    
     return(
         <div>
         <Navbars />
@@ -19,11 +36,11 @@ function Register(){
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Username</Form.Label>
-                    <Form.Control type="test" placeholder="Enter Username" />
+                    <Form.Control type="test" placeholder="Enter Username" onChange={(e)=>setuserName(e.target.value)} />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control type="email" placeholder="Enter email" onChange={(e)=>setEmail(e.target.value)} />
                     <Form.Text className="text-muted">
                     This is form text.
                     </Form.Text>
@@ -31,14 +48,13 @@ function Register(){
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)} />
                 </Form.Group>
-                {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group> */}
-                <Button variant="outline-success" type="submit">
-                    Register
+                
+                <Button variant="outline-success" type="submit" onClick={regis}>
+                Register         
                 </Button>
+
             </Form>
             
         </div>
