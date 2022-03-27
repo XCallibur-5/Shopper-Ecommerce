702758f448f76dfed42e8e3068798e8dcd2e17c9
P <anveshprakhar@outlook.com>
2022-03-27 19:32:14 +0530

    Order dashboard beginning, Orders has the orders made by the user.

--- my-react-app/src/Pages/Order.jsx
+++ my-react-app/src/Pages/Order.jsx
@@ -1,58 +1,30 @@
 import '../App.css';
-import React from "react";
+import React, { useState } from "react";
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 import Navbars from '../Components/Navbars';
 import Container from 'react-bootstrap/esm/Container';
-
-function NavbarOrder (){
-    return (
-        <div className='MyAdminNavbar'>
-            <Navbar bg="light" expand={false} fixed="top">
-            <Navbar.Brand href="#"><h3>         Shopper</h3></Navbar.Brand>
-                <div>
-                    <Button variant="outline-success" className='TopButton'>🎎Login</Button>
-                    <Button variant="outline-success" className='TopButton'>Add Item</Button>
-                </div> 
-            </Navbar>
-        </div>
-    )
-}
-
-// function AdminUsers(){
-
-// }
-// function AdminOrders(){
-    
-// }
-// function AdminProducts(){
-    
-// }
-// function AdminRevenue(){
-    
-// }
+import { userRequest } from "../requestMethods";
+import {useSelector,useDispatch} from 'react-redux';
 
 function OrderDashboard(){
+    const[orders, setOrders]=useState([{}])
+    const currentUser = useSelector((state) => state.user.currentUser);
+    
+    const Orders=async()=>{
+        const res = await userRequest.get(`/orders/find/${currentUser._id}`);
+        setOrders(res.data);
+    }
     return(
         <div>
-        
         < Navbars />
-        {/* <div className='AdminRow'>
-            <div className='AdminCol1'>
-                <AdminUsers />
-            </div>
-            <div className='AdminCol2'>
-                <AdminOrders />
-            </div>
-        </div>
-        <div className='AdminRow'>
-            <div className='AdminCol1'>
-                <AdminProducts />
-            </div>
-            <div className='AdminCol2'>
-                <AdminRevenue />
-            </div>
-        </div> */}
+        <hr />
+        <br />
+        <br />
+        <br /><br /><br /><br /><br />
+        <Button variant="outline-success" type="submit" onClick={Orders}>
+            Show Orders
+        </Button>
     </div> 
     )
      
